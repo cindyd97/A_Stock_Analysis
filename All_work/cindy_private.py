dcfeb1a1bab416d7c522997a208aab0362903b7c
cindawwgg <cindy.duong74@gmail.com>
2025-01-31 20:24:49 -0800

    work

---
+++ All_work/cindy_private.py
@@ -0,0 +1,8 @@
+alpha_advantage_api = "DM2WT6OUIAH7WVGF"
+cindy = 'mongodb+srv://cindyduong74:Chisigma123!@cindy-data-analysis.h2rbq.mongodb.net/'
+fred_account_api = '0f87478926395c0ab1a07fafe0b23cec'
+
+#to be deleted
+'https://medium.com/@lamoriella/data-analytics-project-analyzing-stock-prices-80db2c5b4720'
+'https://medium.com/analytics-vidhya/stock-market-analysis-by-s-p-500-index-using-python-264bffafd235'
+'https://medium.com/@kokhua81/stocks-technical-analysis-ta-with-python-chatgpt-a-comprehensive-guide-871a756ebc7c'
