e3d53aa009e3c271b04f3b01e41560ed7ad323fc
cindawwgg <cindy.duong74@gmail.com>
2025-01-31 17:44:42 -0800

    work

--- Cindy's_work/cindy_private.py
+++ Cindy's_work/cindy_private.py
@@ -5,3 +5,4 @@ fred_account_api = '0f87478926395c0ab1a07fafe0b23cec'
 #to be deleted
 'https://medium.com/@lamoriella/data-analytics-project-analyzing-stock-prices-80db2c5b4720'
 'https://medium.com/analytics-vidhya/stock-market-analysis-by-s-p-500-index-using-python-264bffafd235'
+'https://medium.com/@kokhua81/stocks-technical-analysis-ta-with-python-chatgpt-a-comprehensive-guide-871a756ebc7c'
